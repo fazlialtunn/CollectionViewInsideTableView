8271ad47ff2d4fe7af16f7c1fafc207d38ecc4ae
Fazlı Altun <altunfazl@gmail.com>
2024-03-01 23:25:44 +0300

    first folder structure

--- CollectionViewInsideTableView/Collection Cell/CustomCollectionViewCell.swift
+++ CollectionViewInsideTableView/Collection Cell/CustomCollectionViewCell.swift
@@ -8,5 +8,43 @@
 import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
+    static let identifier = "CustomCollectionViewCell"
     
+    lazy var myLabel: UILabel = {
+        let myLabel = UILabel()
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        return myLabel
+    }()
+    
+    lazy var myImageView: UIImageView = {
+        let myImageView = UIImageView()
+        myImageView.translatesAutoresizingMaskIntoConstraints = false
+        return myImageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .white
+        contentView.addSubview(myLabel)
+        contentView.addSubview(myImageView)
+        NSLayoutConstraint.activate([
+            myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            myImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            myImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            
+            myLabel.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 10),
+            myLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            myLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with model: Model) {
+        self.myLabel.text = model.text
+        self.myImageView.image = UIImage(named: model.imageName)
+    }
 }
--- CollectionViewInsideTableView/ViewController.swift
+++ CollectionViewInsideTableView/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.register(CollectionTableViewCell.self, forCellReuseIdentifier: CollectionTableViewCell.identifier)
+        tableView.backgroundColor = .red
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    var models = [Model]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
+        title = "Hello"
+        setupTable()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func setupTable(){
+        view.addSubview(tableView)
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CollectionTableViewCell.identifier, for: indexPath) as! CollectionTableViewCell
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250.0
+    }
+    
+    
+}
